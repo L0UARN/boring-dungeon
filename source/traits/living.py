7ede8914f40c331e1c247b1a89a63844b672dd27
Louarn <louarn.richard@protonmail.com>
2021-05-05 22:00:30 +0200

    Added common traits used in the game

---
+++ source/traits/living.py
@@ -0,0 +1,41 @@
+"""
+Classes:
+    Living
+"""
+
+
+class Living:
+    """
+    A living is an entity which has health, can be damaged and is capable of healing.
+    """
+    def __init__(self, max_health: int) -> None:
+        """
+        :param max_health: The maximum amount of health the entity can have.
+        """
+        self.max_health = max_health
+        self.health = max_health
+
+    def damage(self, amount: int) -> None:
+        """ Damages the entity (subtracts health).
+
+        :param amount: The amount of damage that will be dealt to the entity.
+        """
+        self.health -= amount
+        if self.health < 0:
+            self.health = 0
+
+    def heal(self, amount: int) -> None:
+        """ Heal the entity (adds health).
+
+        :param amount: The amount of health that will be healed to the entity.
+        """
+        self.health += amount
+        if self.health > self.max_health:
+            self.health = self.max_health
+
+    def is_dead(self) -> bool:
+        """ Get if the entity is dead or not.
+
+        :return: True if the entity is at 0 health or below, or False if not.
+        """
+        return self.health <= 0
---
+++ source/traits/mobile.py
@@ -0,0 +1,81 @@
+"""
+Classes:
+    Mobile
+"""
+
+from source.core.tools import Position, Direction
+
+
+class Mobile:
+    """
+    A mobile entity is an entity which can move inside a graph.
+    """
+    def __init__(self, position: Position, direction: Direction, graph: dict[Position, [Position]]):
+        """
+        :param position: The initial position of the entity inside of the graph.
+        :param direction: The initial orientation of the entity.
+        :param graph: The graph in which the entity will move.
+        """
+        self.position = position
+        self.direction = direction
+        self.graph = graph
+
+    def step_in_direction(self, direction: Direction = None) -> bool:
+        """ Moves 1 step in the specified direction. The entity's direction will be changed to match the direction of
+        the latest step taken.
+
+        :param direction: The direction in which the entity has to step.
+        :return: True if the entity could step, False if not.
+        """
+        if direction is None:
+            direction = self.direction
+        else:
+            self.direction = direction
+
+        if self.position.next_in_direction(direction) in self.graph[self.position]:
+            self.position = self.position.next_in_direction(direction)
+            return True
+        return False
+
+    def move_towards(self, position: Position, teleport: bool = False) -> bool:
+        """ Calculates a path to the specified position, then moves the entity 1 step towards the position if a valid
+        path is found, or teleports the entity to the desired location if teleport is specified and the position is in
+        the graph. The entity's direction will be changed to match the direction of the latest step taken.
+
+        :param position: The position towards which the entity should step.
+        :param teleport: Whether or not the entity should instantly be placed on the position, or should make 1 step.
+        :return: True if the entity could step or teleport, False if not.
+        """
+        if teleport:
+            if position in self.graph:
+                self.direction = self.position.relative_direction(position)
+                self.position = position
+                return True
+            else:
+                return False
+
+        parents = {self.position: None}
+        opened = [self.position]
+        closed = []
+
+        while opened:
+            temp = opened.pop(0)
+            if temp not in closed:
+                closed.append(temp)
+
+            for neighbor in self.graph[temp]:
+                if neighbor not in opened and neighbor not in closed:
+                    opened.append(neighbor)
+                    parents[neighbor] = temp
+
+        path = []
+        point = position
+        while parents[point] is not None:
+            path = [point] + path
+            point = parents[point]
+
+        if path:
+            self.direction = self.position.relative_direction(path[0])
+            self.position = path[0]
+            return True
+        return False
